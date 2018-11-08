fbf9343714980c7b24c6d3f9f38d5c6214ad855d
Yasin Issa Aden <267276@via.dk>
2018-11-08 17:05:39 +0100

    Center class
    Author Jaser and Yasin

---
+++ SEP3Code/FreshFitness/src/model/Center.java
@@ -0,0 +1,107 @@
+package model;
+
+import java.util.ArrayList;
+
+
+public class Center {
+	
+
+	private int ID;
+	private String name;
+	private String address;
+	private ArrayList<User> users;
+	private ArrayList<Activity> activities;
+	
+	public Center(int ID, String name, String address)
+	{
+		this.ID=ID;
+		this.name=name;
+		this.address=address;
+		users = new ArrayList<User>();
+		activities = new ArrayList<Activity>();
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public ArrayList<Activity> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(ArrayList<Activity> activities) {
+		this.activities = activities;
+	}
+	
+	public void registerUser(User user)
+	{
+		users.add(user);
+	}
+	
+	public void unRegisterUser(User user)
+	{
+		users.remove(user);
+	}
+	
+	public ArrayList<User> getAllUser()
+	{
+		return users;
+	}
+	
+	public void registerActivity(Activity activity, User user)
+	{
+		user = new User(ID, address, address, address, ID, null, null);
+		user.addActivity(activity);	
+	}
+	
+	public ArrayList<Activity> getAllActivities()
+	{
+		return activities;
+	}
+	
+	public int getNumberOfActivitiesByName(String name)
+	{
+		int count=0;
+		for(int i = 0; i < activities.size(); i++)
+		{
+			if(activities.get(i).getName().equals(name))
+			{
+				count++;
+			}
+		}
+		return count;
+	}	
+	
+	public int getNumberOfActivitiesByDate(MyDate date)
+	{
+		int count=0;
+		for(int i = 0; i < activities.size(); i++)
+		{
+			if(activities.get(i).getDate().equals(date))
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	@Override
+	public String toString() {
+		return "Center [ID=" + ID + ", name=" + name + ", address=" + address + ", users=" + users + ", activities="
+				+ activities + "]";
+	}
+	
+	 
+}
--- SEP3Code/FreshFitness/src/model/User.java
+++ SEP3Code/FreshFitness/src/model/User.java
@@ -34,6 +34,94 @@ public class User
 		workouts = new ArrayList<Workout>();
 	}
 	
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public MyDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(MyDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public EUserRole getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(EUserRole userRole) {
+		this.userRole = userRole;
+	}
+
+	public ArrayList<Activity> getActivities() {
+		return activities;
+	}
+
+	public void setActivities(ArrayList<Activity> activities) {
+		this.activities = activities;
+	}
+
+	public Subscription getSubscription() {
+		return subscription;
+	}
+
+	public void setSubscription(Subscription subscription) {
+		this.subscription = subscription;
+	}
+
+	public ArrayList<Workout> getWorkouts() {
+		return workouts;
+	}
+
+	public void setWorkouts(ArrayList<Workout> workouts) {
+		this.workouts = workouts;
+	}	
+	
 	public ArrayList<Activity> getAllActivities()
 	{
 		return activities;
@@ -52,6 +140,16 @@ public class User
 	public void removeWorkout(Workout workout)
 	{
 		workouts.remove(workout);
+	}
+	
+	public void addActivity(Activity activity)
+	{
+		activities.add(activity);
+	}
+	
+	public void removeActivity(Activity activity)
+	{
+		activities.remove(activity);
 	}
 	
 	public ArrayList<Workout> getAllWorkouts()
